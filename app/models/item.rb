9a0116540b40af2ac0c7884338039865b7993331
James Fox-Collis <jlfoxcollis@gmail.com>
2021-01-07 18:34:24 -0700

    Finished model testing for issue #165

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,5 +2,16 @@ class Item < ApplicationRecord
   belongs_to :merchant
   has_many :invoice_items
   has_many :invoices, through: :invoice_items
+  has_many :transactions, through: :invoices
   enum status: [ :disabled, :enabled ]
+
+
+  def self.top_5_items
+    joins(:transactions)
+    .select('items.*, sum(invoice_items.quantity * invoice_items.unit_price) AS total_revenue')
+    .where('transactions.result = ?', 1)
+    .group('items.id')
+    .order('total_revenue DESC')
+    .limit(5)
+  end
 end
