274b6ec04ce0f39e9cd28b1c9ebc6dfd755ac2bc
Ely Hess <Ely.hess@me.com>
2021-01-09 11:32:54 -0700

    Add sad path test case

--- spec/features/admin/merchants/edit_spec.rb
+++ spec/features/admin/merchants/edit_spec.rb
@@ -14,6 +14,14 @@ RSpec.describe 'Admin Merchants Show' do
       expect(page).to have_content('Test')
       expect(page).to have_content("Merchant Test was updated successfully!")
     end
-    
+
+    it 'displays flash message if validations fail' do
+      visit edit_admin_merchant_path(@merchant_1)
+      fill_in 'merchant[name]', with: ''
+      click_on 'Update Merchant'
+
+      expect(current_path).to eq(admin_merchant_path(@merchant_1))
+      expect(page).to have_content("[\"Name can't be blank\"]")
+    end
   end
 end
