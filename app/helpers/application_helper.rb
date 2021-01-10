1fc6c8e8b0aebb948a02189dc3b00a60b8e8a5ed
Ely Hess <Ely.hess@me.com>
2021-01-10 11:14:11 -0700

    Add devise helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,20 @@ module ApplicationHelper
     x.insert(0,['', '']) if include_blank == true
     x
   end
+
+  def resource_name
+    :user
+  end
+  
+  def resource
+    @resource ||= User.new
+  end
+  
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
+  
+  def resource_class
+    devise_mapping.to
+  end
 end
