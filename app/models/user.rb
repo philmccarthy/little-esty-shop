c8112dea1ac510a9b4f0ae9ff212b823ddf0917f
Ely Hess <Ely.hess@me.com>
2021-01-10 11:15:02 -0700

    add has_one relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_one :merchants
+  has_one :merchant, dependent: :destroy
   
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
