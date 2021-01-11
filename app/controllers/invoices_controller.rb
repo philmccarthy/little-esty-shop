22d40b5fc2e36bce23a19b57b8c7927c2eba9a7e
Phil McCarthy <philmcc2631@gmail.com>
2021-01-10 17:13:41 -0700

    Update styling for item & invoice show, add set_merchant for invoices#show and #index

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -1,9 +1,15 @@
 class InvoicesController < ApplicationController
+  before_action :set_merchant, only: [:index, :show]
+
   def index
-    @merchant = Merchant.find(params[:merchant_id])
   end
   
   def show
     @invoice = Invoice.find(params[:id])
+  end
+
+  private
+  def set_merchant
+    @merchant = Merchant.find(params[:merchant_id])
   end
 end
