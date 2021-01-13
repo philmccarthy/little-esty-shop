62f0d94c1449c49d64327696dd91def46ad439ce
Sam Yeo <yeosamuel95@gmail.com>
2021-01-13 13:22:33 -0700

    Change spacing

--- config/routes.rb
+++ config/routes.rb
@@ -1,30 +1,30 @@
 Rails.application.routes.draw do
   scope "(:lang)", locale: /#{I18n.available_locales.join("|")}/ do
-  root to: "welcome#index"
-  #scope "(:lang)", locale: /#{I18n.available_locales.join("|")}/ do
+    root to: "welcome#index"
+    #scope "(:lang)", locale: /#{I18n.available_locales.join("|")}/ do
 
-  devise_for :users, controllers: {:sessions => "users/sessions", :passwords => "users/passwords", :registrations => "users/registrations"}
-  resources :users, only: [:show]
-  resources :customers, only: [:show]
-  resources :welcome, only: [:index]
-  resources :cart, only: [:show, :update, :destroy]
-  resources :orders, only: [:create, :show]
+    devise_for :users, controllers: {:sessions => "users/sessions", :passwords => "users/passwords", :registrations => "users/registrations"}
+    resources :users, only: [:show]
+    resources :customers, only: [:show]
+    resources :welcome, only: [:index]
+    resources :cart, only: [:show, :update, :destroy]
+    resources :orders, only: [:create, :show]
 
-  namespace :admin do
-    resources :merchants, except: [:destroy]
-    resources :merchants_status, only: [:update]
-    resources :invoices, only: [:index, :show, :update]
-  end
+    namespace :admin do
+      resources :merchants, except: [:destroy]
+      resources :merchants_status, only: [:update]
+      resources :invoices, only: [:index, :show, :update]
+    end
 
-  resources :merchants, module: :merchant do
-    resources :items
-    resources :items_status, controller: "merchant_items_status", only: [:update]
-    resources :invoices
-    resources :invoice_items, only: [:update]
-    resources :dashboard, only: [:index]
-  end
+    resources :merchants, module: :merchant do
+      resources :items
+      resources :items_status, controller: "merchant_items_status", only: [:update]
+      resources :invoices
+      resources :invoice_items, only: [:update]
+      resources :dashboard, only: [:index]
+    end
 
 
-  resources :admin, controller: 'admin/dashboard', only: [:index]
+    resources :admin, controller: 'admin/dashboard', only: [:index]
   end
 end
