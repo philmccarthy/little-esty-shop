d645bb3f9de57ab1ce96b863af4874616e7e6838
Phil McCarthy <philmcc2631@gmail.com>
2021-01-18 10:11:42 -0700

    Add associations to match tests re: previous commit

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -1,6 +1,8 @@
 class InvoiceItem < ApplicationRecord
   belongs_to :item
+  has_one :merchant, through: :item
   belongs_to :invoice
+  has_many :transactions, through: :invoice
 
   enum status: [ :pending, :packaged, :shipped ]
 end
