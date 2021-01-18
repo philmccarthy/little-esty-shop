c9e284fb4be6b83701c8a15de22fd9041d86df86
Phil McCarthy <philmcc2631@gmail.com>
2021-01-18 10:11:09 -0700

    Add invoice_item model associations for merchant and transactions

--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -7,6 +7,8 @@ RSpec.describe InvoiceItem, type: :model do
   describe 'relationships' do
     it {should belong_to :item}
     it {should belong_to :invoice}
+    it {should have_one(:merchant).through(:item)}
+    it {should have_many(:transactions).through(:invoice)}
   end
 
   describe 'instance methods' do
