082834478b9f0ab1b5d4067dcb09e56a6d98ae3c
Phil McCarthy <philmcc2631@gmail.com>
2021-01-19 12:22:52 -0700

    Add invoice_item methods find_max_discount and apply_max_discount

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -5,4 +5,21 @@ class InvoiceItem < ApplicationRecord
   has_many :transactions, through: :invoice
 
   enum status: [ :pending, :packaged, :shipped ]
+
+  after_save :apply_max_discount
+
+  def apply_max_discount
+    inv_item = InvoiceItem.find_max_discount(self.id)
+    if inv_item
+      inv_item.update_columns(unit_price: (inv_item.unit_price * (1 - inv_item.discount.to_f / 100)))
+    end
+  end
+
+  def self.find_max_discount(id)
+    joins(merchant: :bulk_discounts).
+    select('invoice_items.*, bulk_discounts.pct_discount AS discount').
+    where("invoice_items.id = ? AND invoice_items.quantity >= bulk_discounts.min_qty", id).
+    order('bulk_discounts.pct_discount DESC').
+    limit(1).first
+  end
 end
