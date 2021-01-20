e7c32073e1cff8eb847b7d161ee87503ed72de02
Phil McCarthy <philmcc2631@gmail.com>
2021-01-19 17:30:37 -0700

    Add model tests for invoice_item discounted? and with_discount inst. methods

--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -15,9 +15,9 @@ RSpec.describe InvoiceItem, type: :model do
     before(:each) do
       @user = create(:user, role: 1)
       @merchant = create(:merchant, user: @user)
-      create(:bulk_discount, pct_discount: 10, min_qty: 50, merchant: @merchant)
-      create(:bulk_discount, pct_discount: 20, min_qty: 101, merchant: @merchant)
-      create(:bulk_discount, pct_discount: 5, min_qty: 20, merchant: @merchant)
+      @discount_1 = create(:bulk_discount, pct_discount: 10, min_qty: 50, merchant: @merchant)
+      @discount_2 = create(:bulk_discount, pct_discount: 20, min_qty: 101, merchant: @merchant)
+      @discount_3 = create(:bulk_discount, pct_discount: 5, min_qty: 20, merchant: @merchant)
       
       @user1 = create(:user, role: 1)
       @customer_1 = create(:customer, user: @user1)
@@ -46,8 +46,26 @@ RSpec.describe InvoiceItem, type: :model do
         
         inv_item_2 = InvoiceItem.find_max_discount(@inv_item_2.id)
         inv_item_2.apply_max_discount
-        
+
         expect(inv_item_2.unit_price).to eq(20)
+      end
+      
+      it '#discounted?' do
+        inv_item_1 = InvoiceItem.find(@inv_item_1.id)
+        inv_item_2 = InvoiceItem.find(@inv_item_2.id)
+
+        expect(inv_item_1.discounted?).to be_truthy
+        expect(inv_item_2.discounted?).to be_truthy
+      end
+
+      it '#with_discount' do
+        inv_item = @inv_item_1.with_discount
+        expect(inv_item.discount).to eq(10)
+        expect(inv_item.discount_applied).to eq(@discount_1.id)
+
+        inv_item_2 = @inv_item_2.with_discount
+        expect(inv_item_2.discount).to eq(20)
+        expect(inv_item_2.discount_applied).to eq(@discount_2.id)
       end
     end
   end
