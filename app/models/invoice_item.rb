a2c996064c3a5e2f64951f3466cecd12c8751f44
Phil McCarthy <philmcc2631@gmail.com>
2021-01-19 17:31:09 -0700

    Add discounted? and with_discount methods to invoice_item

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -9,15 +9,26 @@ class InvoiceItem < ApplicationRecord
   after_save :apply_max_discount
 
   def apply_max_discount
-    inv_item = InvoiceItem.find_max_discount(self.id)
+    inv_item = self.with_discount
     if inv_item
-      inv_item.update_columns(unit_price: (inv_item.unit_price * (1 - inv_item.discount.to_f / 100)))
+      inv_item.update_columns(
+        unit_price: (inv_item.unit_price * (1 - inv_item.discount.to_f / 100)),
+        discount_applied: inv_item.discount_id
+      )
     end
+  end
+
+  def with_discount
+    InvoiceItem.find_max_discount(self.id)
+  end
+
+  def discounted?
+    !discount_applied.nil?
   end
 
   def self.find_max_discount(id)
     joins(merchant: :bulk_discounts).
-    select('invoice_items.*, bulk_discounts.pct_discount AS discount').
+    select('invoice_items.*, bulk_discounts.pct_discount AS discount, bulk_discounts.id AS discount_id').
     where("invoice_items.id = ? AND invoice_items.quantity >= bulk_discounts.min_qty", id).
     order('bulk_discounts.pct_discount DESC').
     limit(1).first
